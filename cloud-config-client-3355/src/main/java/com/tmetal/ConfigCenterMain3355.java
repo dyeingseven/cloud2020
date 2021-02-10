a2df3f69756334b64df3e18615cb8931a7af4d75
dyeingseven <dyeingseven@gmail.com>
2021-02-10 10:15:33 +0800

    提交项目的分布式分配至中心客户端和服务端（修改名字）

--- cloud-config-client-3355/src/main/java/com/tmetal/ConfigCenterMain3355.java
+++ cloud-config-client-3355/src/main/java/com/tmetal/ConfigCenterMain3355.java
@@ -2,12 +2,12 @@ package com.tmetal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-class ConfigServerMain3344 {
+@EnableEurekaClient
+public class ConfigCenterMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerMain3344.class, args);
+        SpringApplication.run(ConfigCenterMain3355.class, args);
     }
 }
